7f89c28721710c31d46959b4b5424dc211c89eed
Jaan Smith <jvonrochow@gmail.com>
2019-12-05 09:59:23 +1100

    remove console logs

--- 2019/4/1/passwords.js
+++ 2019/4/1/passwords.js
@@ -19,10 +19,8 @@ async function checkHasDoubles(passwd) {
 function isLeftLowerOrEqThanRight(char1, char2) {
   return new Promise(function(resolve, reject) {
     if (parseInt(char1) <= parseInt(char2)) {
-      //console.log("SUCCESS " + char1 + " should be lower or equal to " + char2);
       resolve(true);
     } else {
-      //console.log("FAIL " + char1 + " should be lower or equal to " + char2);
       resolve(false);
     }
   });
@@ -31,14 +29,11 @@ function isLeftLowerOrEqThanRight(char1, char2) {
 async function decreaseIsFalse(str) {
   return new Promise(async function(resolve, reject) {
     for (var i = 0; i < str.length - 1; i++) {
-      //console.log("how many");
       var boolCheck = await isLeftLowerOrEqThanRight(str[i], str[i + 1]);
       if (!boolCheck) {
-        //console.log("resolving false with", str[i], str[i + 1]);
         resolve(false);
       }
     }
-    //console.log("resolving true");
     resolve(true);
   });
 }
@@ -47,24 +42,17 @@ async function checkHasDecrease(passwd) {
   return new Promise(async function(resolve, reject) {
     var str = passwd.toString();
     var bool = await decreaseIsFalse(str);
-    //console.log(!bool);
     resolve(!bool);
   });
 }
 
-//If all tests are passed, resolve to true
-//Checks should be structured so all resolve to true if failed
 async function assess(passwd) {
   return new Promise(async function(resolve, reject) {
     var checkDecrease = await checkHasDecrease(passwd);
     var checkHasDouble = await checkHasDoubles(passwd);
     if (passwd.toString().length != 6 || !checkHasDouble || checkDecrease) {
-      //console.log("uh oh");
-
       resolve(false);
     } else {
-      //console.log("all good!");
-
       resolve(true);
     }
   });
@@ -74,9 +62,7 @@ async function main() {
   var counter = 0;
   for (var i = 254032; i <= 789860; i++) {
     var blah = await assess(i);
-    //console.log(blah);
     if (blah) {
-      //console.log("here");
       counter = counter + 1;
     }
   }
