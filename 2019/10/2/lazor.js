57fa80f888a5c34e6542a5459009c2d49e8de8e2
Jaan Smith <smithjp@rba.gov.au>
2019-12-29 02:03:12 +1100

    day 10 part 2 done

--- 2019/10/2/lazor.js
+++ 2019/10/2/lazor.js
@@ -1,12 +1,9 @@
 var fs = require("fs");
 var os = require("os");
 
-
-
 function splitData(data) {
   return data.toString().split(os.EOL);
 }
-
 
 function splitNLToArray(array) {
   return new Promise(function(resolve, reject) {
@@ -16,46 +13,66 @@ function splitNLToArray(array) {
   });
 }
 
-function countAsteroids(x1, y1, array) {
-  var used = new Map();
-  var count = 0;
-
+function populateAsteroids(x1, y1, array) {
+  var asteroids = [];
   for(var y = 0; y < array.length; y++) {
     for(var x = 0; x < array[y].length; x++) {
       if(array[y][x] === '#' && (x !== x1 ||  y !== y1)) {
-        var a = Math.atan2(y - y1, x - x1) * 180 / Math.PI;
-        if(used.get(a) === undefined) {
-          used.set(a, true);
-            count++;
-        }
-      } 
+        var a = Math.atan2(y - y1,x - x1) * 180 / Math.PI;
+        asteroids.push({x: x, y: y, angle: a, distance: Math.abs(x-x1+y-y1)})
+      }
+    } 
+  }
+  asteroids.sort(
+    function(a,b) {
+      if(a.angle === b.angle) {
+        return a.distance - b.distance;
+      }
+        return a.angle < b.angle ? -1 : 1;
+    }
+  )
+  return asteroids;
+}
+
+function getUpwardsIndex(asteroids) {
+  for(var i = 0; i < asteroids.length; i++) {
+    if(asteroids[i].angle >= -90) {
+      return i;
     }
   }
+}
 
-  return count;
+function vaporise(asteroids) {
+  var count = 0;
+  var index = 0;
+  var angle = 1000;
+  while(count <= 200) {
+    if(index >= asteroids.length) {
+      index = 0;
+    }
+    if(asteroids[index].angle !== angle) {
+      angle = asteroids[index].angle;
+      count++;
+      if(count === 200) {
+        return {x: asteroids[index].x, y: asteroids[index].y}
+        break;
+      }
+      asteroids.splice(index,1);
+    } else {
+      index++;
+    }
+  }
 }
 
 async function main() {
-  var mapStream = fs.createReadStream("tmap.txt");
+  var mapStream = fs.createReadStream("map.txt");
   var splitNewLines = await splitNLToArray(mapStream);
-  var xc = 0, yc = 0, max = 0;
-
-  for(var y = 0; y < splitNewLines.length; y++) {
-    for(var x = 0; x < splitNewLines[y].length; x++) {
-      if(splitNewLines[y][x] === '#') {
-        var mv = countAsteroids(x, y, splitNewLines);
-        if(mv > max) {
-          max = mv;
-          xc = x;
-          yc = y;
-        }
-      }
-    }
-  }
-
-  console.log("Coordinate of prime location for station is x: ", xc,  " y: ", yc, ".");
-  console.log("Number of asteroids viewable from this location are: ", max);
-
+  var asteroids = populateAsteroids(20,19,splitNewLines); //20,19 for main //11,13 for test
+  var index = getUpwardsIndex(asteroids);
+  asteroids = asteroids.slice(index,asteroids.length).concat(asteroids.slice(0,index));
+  var {x, y} = vaporise(asteroids);
+  console.log("200th asteroid's x & y: ", x, ' ', y);
+  console.log(x * 100 + y);
 }
 
 main();
