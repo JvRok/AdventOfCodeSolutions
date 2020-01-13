963c87312922374fe9658261f6e582d5a64d8ffb
Jaan Smith <jvonrochow@gmail.com>
2020-01-13 15:24:34 +1100

    Alternate Part 1 to day 14

--- 2019/14/1/fuelcalc.js
+++ 2019/14/1/fuelcalc.js
@@ -18,7 +18,6 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 function splitNLToArray(array) {
   return new Promise(function(resolve, reject) {
     array.on("data", function(data) {
@@ -49,40 +48,73 @@ function splitFuel(reactionsLine) {
 }
 
 function requirements(resource, req) {
-
-  //This variable is the map 
+  //This variable is the map
+  //await sleep(1000);
+  //console.log(resourcesMap);
+  //console.log("#############################");
   var reactionMap = fuelMap.get(resource);
+  if (resource !== "ORE") {
+    var num_of_reactions = Math.ceil(req / reactionMap.product.n);
+  }
   var breakGate = true;
   //console.log(resource, reactionMap);
-  if(resourcesMap.get("FUEL") === 1) {
+  if (resourcesMap.get("FUEL") === req) {
     return;
-  } else if (resource === "ORE" ) {
+  } else if (resource === "ORE") {
     //console.log("In with the ore loop");
     //Work out how much ore you need
-    var requiredOre = req - resourcesMap.get("ORE")
+    var requiredOre = req;
     //console.log("requiredOre", requiredOre);
-    resourcesMap.set(resource, resourcesMap.get(resource) + requiredOre); 
+    resourcesMap.set(resource, resourcesMap.get(resource) + requiredOre);
     countMap.set(resource, countMap.get(resource) + requiredOre);
   } else {
-    for(var i = 0; i < reactionMap.fuel.length; i++) {
-      if(reactionMap.fuel[i].n <= resourcesMap.get(reactionMap.fuel[i].r)) {
+    for (var i = 0; i < reactionMap.fuel.length; i++) {
+      //console.log("num_of_reactions: ", num_of_reactions);
+      var roundUpRequired = num_of_reactions * reactionMap.fuel[i].n;
+      //console.log("Reaction Map: ", reactionMap.fuel[i]);
+      /*console.log(
+        "reaction map fuel for ",
+        reactionMap.fuel[i].r,
+        " needed for reaction = ",
+        reactionMap.fuel[i].n,
+        " required amount of product ",
+        reactionMap.product.p,
+        " needed = ",
+        req,
+        " which means requird amount of input equals: ",
+        roundUpRequired
+      );*/
+      if (
+        reactionMap.fuel[i].n * num_of_reactions <=
+        resourcesMap.get(reactionMap.fuel[i].r)
+      ) {
       } else {
-        //console.log("We didnt have enough resources for the reaction, lets recurse");
+        /*console.log(
+          "We didnt have enough resources for the reaction, lets recurse"
+        );*/
         breakGate = false;
-        requirements(reactionMap.fuel[i].r,reactionMap.fuel[i].n);
+        requirements(
+          reactionMap.fuel[i].r,
+          roundUpRequired - resourcesMap.get(reactionMap.fuel[i].r)
+        );
       }
     }
-    if(breakGate) {
-      resourcesMap.set(resource, resourcesMap.get(resource) + reactionMap.product.n);
-      for(var i = 0; i < reactionMap.fuel.length; i++) {
-        resourcesMap.set(reactionMap.fuel[i].r, resourcesMap.get(reactionMap.fuel[i].r) - reactionMap.fuel[i].n) 
+    if (breakGate) {
+      //console.log(reactionMap.product.n);
+      resourcesMap.set(
+        resource,
+        resourcesMap.get(resource) + reactionMap.product.n * num_of_reactions
+      );
+      for (var i = 0; i < reactionMap.fuel.length; i++) {
+        //console.log("num_of_reactions: ", num_of_reactions);
+        var roundUpRequired = num_of_reactions * reactionMap.fuel[i].n;
+        resourcesMap.set(
+          reactionMap.fuel[i].r,
+          resourcesMap.get(reactionMap.fuel[i].r) - roundUpRequired
+        );
       }
     }
   }
-
-
-
-
 }
 async function main() {
   var input = fs.createReadStream("input.txt");
@@ -94,10 +126,8 @@ async function main() {
     splitFuel(reactions);
   }
   //TODO
-  while(resourcesMap.get("FUEL") < 1) {
-    //console.log(resourcesMap);
-    //console.log("#############################");
-    //await sleep(2000);
+  while (resourcesMap.get("FUEL") < 1) {
+    //await sleep(500);
     requirements("FUEL", 1);
   }
   console.log(resourcesMap);
