227315396f94aca65a81ed28083426b0e8be7bd6
Jaan Smith <jvonrochow@gmail.com>
2020-03-06 17:02:13 +1100

    dijkstra maze solver

---
+++ assorted_snippets/convertMazeToGraph.js
@@ -0,0 +1,90 @@
+var fs = require("fs");
+var os = require("os");
+
+function splitData(data) {
+  return data.toString().split(os.EOL);
+}
+
+function splitArray(input) {
+  return new Promise(function(resolve, reject) {
+    input.on("data", function(data) {
+      resolve(splitData(data));
+    });
+  });
+}
+
+function getMazeVal(mazeObject, y, x) {
+  if (mazeObject[0].length > x && x >= 0 && mazeObject.length > y && y >= 0) {
+    if (mazeObject[y][x] === "#") {
+      return 999999999;
+    } else if (mazeObject[y][x] === "S") {
+      return 0;
+    } else {
+      return Infinity;
+    }
+  } else {
+    return -1;
+  }
+}
+
+function generateLinks(mazeObject, y, x) {
+  var links = {};
+  links["x" + (x - 1) + "y" + y] = getMazeVal(mazeObject, y, x - 1);
+  links["x" + (x + 1) + "y" + y] = getMazeVal(mazeObject, y, x + 1);
+  links["x" + x + "y" + (y - 1)] = getMazeVal(mazeObject, y - 1, x);
+  links["x" + x + "y" + (y + 1)] = getMazeVal(mazeObject, y + 1, x);
+  for (obj in links) {
+    if (links[obj] === -1) {
+      delete links[obj];
+    } else if (links[obj] === Infinity) {
+      links[obj] = 1;
+    }
+  }
+  return links;
+}
+
+function createObject(mazeObject, y, x) {
+  var links = generateLinks(mazeObject, y, x);
+  var val = getMazeVal(mazeObject, y, x);
+  var coord = { x: x, y: y };
+  var path = "";
+  if (val === 0) {
+    path = "S";
+  }
+  return {
+    val: val,
+    symbol: mazeObject[y][x],
+    coord: coord,
+    links: links,
+    proc: false,
+    path: path
+  };
+}
+
+//assumes maze is rectangular - each line is a string of chars
+//# = wall, . = open space, S = Start, E = End (goal)
+function convertMazeObjectToGraph(mazeObject) {
+  var mazeGraph = {};
+  for (var x = 0; x < mazeObject[0].length; x++) {
+    for (var y = 0; y < mazeObject.length; y++) {
+      mazeGraph["x" + x + "y" + y] = createObject(mazeObject, y, x);
+    }
+  }
+  return mazeGraph;
+}
+
+async function getMaze() {
+  var input = fs.createReadStream("input.txt");
+  var mazeObject = await splitArray(input);
+  return mazeObject;
+}
+
+async function getGraph() {
+  var input = fs.createReadStream("input.txt");
+  var mazeObject = await splitArray(input);
+  var mazeGraph = await convertMazeObjectToGraph(mazeObject);
+  return mazeGraph;
+}
+
+exports.getGraph = getGraph();
+exports.getMaze = getMaze();
--- assorted_snippets/dijkstra.js
+++ assorted_snippets/dijkstra.js
@@ -1,15 +1,26 @@
-var graph = {
-  A: { B: 2, C: 4, K: 5, val: 0, proc: false, path: "A" },
-  B: { C: 1, D: 7, J: 4, G: 1, val: Infinity, proc: false, path: "" },
-  C: { B: 1, E: 3, J: 3, val: Infinity, proc: false, path: "" },
-  D: { E: 2, F: 4, val: Infinity, proc: false, path: "" },
-  E: { D: 2, F: 7, val: Infinity, proc: false, path: "" },
-  F: { val: Infinity, proc: false, path: "" },
-  G: { D: 6, I: 7, H: 1, val: Infinity, proc: false, path: "" },
-  H: { D: 7, val: Infinity, proc: false, path: "" },
-  I: { H: 2, F: 4, val: Infinity, proc: false, path: "" },
-  J: { D: 7, E: 2, val: Infinity, proc: false, path: "" },
-  K: { F: 10, E: 3, val: Infinity, proc: false, path: "" }
+var mc = require("./convertMazeToGraph");
+
+//Empty object to load graph into
+var graph = {};
+
+//Example Graph
+var exgraph = {
+  A: { links: { B: 2, C: 4, K: 5 }, val: 0, proc: false, path: "A" },
+  B: {
+    links: { C: 1, D: 7, J: 4, G: 1 },
+    val: Infinity,
+    proc: false,
+    path: ""
+  },
+  C: { links: { B: 1, E: 3, J: 3 }, val: Infinity, proc: false, path: "" },
+  D: { links: { E: 2, F: 4 }, val: Infinity, proc: false, path: "" },
+  E: { links: { D: 2, F: 7 }, val: Infinity, proc: false, path: "" },
+  F: { links: {}, val: Infinity, proc: false, path: "" },
+  G: { links: { D: 6, I: 7, H: 1 }, val: Infinity, proc: false, path: "" },
+  H: { links: { D: 7 }, val: Infinity, proc: false, path: "" },
+  I: { links: { H: 2, F: 4 }, val: Infinity, proc: false, path: "" },
+  J: { links: { D: 7, E: 2 }, val: Infinity, proc: false, path: "" },
+  K: { links: { F: 10, E: 3 }, val: Infinity, proc: false, path: "" }
 };
 
 function getLowestAvailVal() {
@@ -20,28 +31,79 @@ function getLowestAvailVal() {
       la.val = graph[obj].val;
     }
   }
+  //console.log(la);
   return la;
 }
 
 function updateNeighbours(la) {
-  for (obj in graph[la.n]) {
-    if (obj !== "val" && obj !== "proc" && obj !== "path") {
-      //console.log(obj, ": ", graph[obj].val);
-      //console.log(la.n, ": ", graph[la.n].val + graph[la.n][obj]);
-
-      if (graph[obj].val > graph[la.n][obj] + graph[la.n].val) {
-        graph[obj].val = graph[la.n][obj] + graph[la.n].val;
-        graph[obj].path = graph[la.n].path + obj;
-      }
+  for (obj in graph[la.n].links) {
+    //console.log(graph[obj].val, ">", graph[la.n].links[obj] + graph[la.n].val);
+    if (graph[obj].val > graph[la.n].links[obj] + graph[la.n].val) {
+      graph[obj].val = graph[la.n].links[obj] + graph[la.n].val;
+      graph[obj].path = graph[la.n].path + " -> " + obj;
     }
   }
   graph[la.n].proc = true;
 }
 
-var la = getLowestAvailVal();
-while (la.val !== Infinity) {
-  updateNeighbours(la);
-  la = getLowestAvailVal();
+function getFinishCoord() {
+  for (const obj in graph) {
+    if (graph[obj].symbol === "E") {
+      return obj;
+    }
+  }
 }
 
-console.log(graph);
+function parsePathToCoordinateArray(path) {
+  var path = path.trim();
+  var pathCoords = path.split("->");
+  pathCoords.shift();
+  for (var i = 0; i < pathCoords.length; i++) {
+    pathCoords[i] = pathCoords[i].trim();
+    pathCoords[i] = pathCoords[i].slice(1);
+    pathCoords[i] = pathCoords[i].split("y");
+    pathCoords[i] = {
+      y: parseInt(pathCoords[i][1]),
+      x: parseInt(pathCoords[i][0])
+    };
+  }
+  console.log(pathCoords);
+  return pathCoords;
+}
+
+function drawMaze(maze, path) {
+  var coordArray = parsePathToCoordinateArray(path);
+  var mazeArray = maze;
+  for (var i = 0; i < coordArray.length - 1; i++) {
+    var x = coordArray[i].x;
+    var y = coordArray[i].y;
+
+    mazeArray[y] = mazeArray[y].slice(0, x) + "*" + mazeArray[y].slice(x + 1);
+    console.log(mazeArray[y][x]);
+  }
+  console.log(mazeArray);
+}
+
+async function main() {
+  graph = await mc.getGraph;
+  maze = await mc.getMaze;
+  var finishCoord = getFinishCoord();
+  console.log(finishCoord);
+  //console.log(graph);
+
+  var la = getLowestAvailVal();
+  while (la.val !== Infinity) {
+    updateNeighbours(la);
+    la = getLowestAvailVal();
+  }
+  //console.log(graph);
+  drawMaze(maze, graph[finishCoord].path);
+  console.log(
+    "Optimal Path is: ",
+    graph[finishCoord].path,
+    " which has a total path weight of: ",
+    graph[finishCoord].val
+  );
+}
+
+main();
